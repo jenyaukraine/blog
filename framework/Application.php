ec921c2897dfaf2acf2a7cbe30f4978a352686d8
dimmask <deavenor.encarna@gmail.com>
2016-01-31 12:06:40 +0200

    Init commit

---
+++ framework/Application.php
@@ -0,0 +1,22 @@
+<?php
+/**    
+ * Application.php
+ * 
+ * PHP version 5
+ *
+ * @category   Category Name
+ * @package    Package Name
+ * @subpackage Subpackage name
+ * @author     dimmask <ddavidov@mindk.com>
+ * @copyright  2011-2013 mindk (http://mindk.com). All rights reserved.
+ * @license    http://mindk.com Commercial
+ * @link       http://mindk.com
+ */
+
+
+namespace Framework;
+
+
+class Application {
+
+} 
---
+++ framework/Loader.php
@@ -0,0 +1,52 @@
+<?php
+
+class Loader{
+
+	protected static $instance = null;
+	protected static $namespaces = array();
+
+	public static function getInstance(){
+		if(empty(self::$instance)){
+			self::$instance = new self();
+		}
+
+		return self::$instance;
+	}
+
+	public static function load($classname){
+		//
+		$path = str_replace('Framework','',$classname);
+		$path = __DIR__ . str_replace("\\","/", $path) . '.php';
+
+		if(file_exists($path)){
+			include_once($path);
+		}
+	}
+
+	public static function load2($classname){
+		//
+		$path = str_replace('Framework','',$classname);
+		$path = __DIR__ . str_replace("\\","/", $path) . '.php';
+
+		if(file_exists($path)){
+			include_once($path);
+		}
+
+	}
+
+	private function __construct(){
+		// Init
+		spl_autoload_register(array(__CLASS__, 'load'));
+		spl_autoload_register(array(__CLASS__, 'load2'));
+	}
+
+	private function __clone(){
+		// lock
+	}
+
+	public static function addNamespacePath($namespace, $path){
+		//...
+	}
+}
+
+Loader::getInstance();
