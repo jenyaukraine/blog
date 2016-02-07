d855ff1b0ffa42a00f97a1b36773f74d29153578
dimmask <deavenor.encarna@gmail.com>
2016-02-07 15:58:00 +0200

    Pouter small updates

--- framework/Router/Router.php
+++ framework/Router/Router.php
@@ -29,14 +29,24 @@ class Router{
 
 		$route_found = null;
 		echo "<pre>";
+
 		foreach(self::$map as $route){
+
 			$pattern = $this->prepare($route);
 
 			echo $pattern.'<br />';
 
-			if(preg_match($pattern, $url)){
+			if(preg_match($pattern, $url, $params)){
+
+				// Get assoc array of params:
+				preg_match($pattern, str_replace(array('{','}'), '', $route['pattern']), $param_names);
+				$params = array_map('urldecode', $params);
+				$params = array_combine($param_names, $params);
+				array_shift($params); // Get rid of 0 element
+
 				$route_found = $route;
-				$route_found['params'] = array(); // values
+				$route_found['params'] = $params;
+
 				break;
 			}
 
@@ -51,11 +61,16 @@ class Router{
 
 	private function prepare($route){
 
-		$pattern = preg_replace('~\{[\w\d_]+\}~Ui','[\w\d_]+', $route['pattern']);
+		$pattern = preg_replace('~\{[\w\d_]+\}~Ui','([\w\d_]+)', $route['pattern']);
 
 		$pattern = '~^'. $pattern.'$~';
 
 		return $pattern;
+	}
+
+	private function chunkVars(){
+		$route_pattern = self::routeToRegexp($route['route']);
+
 	}
 }
  
